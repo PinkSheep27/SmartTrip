63d74ee8b1866fecd905c8801923235aaf1fde31
Aaron Siu <99273246+asiu3209@users.noreply.github.com>
2025-10-16 10:50:25 -0400

    Added form and a heading for dining page

--- src/components/DiningPage.tsx
+++ src/components/DiningPage.tsx
@@ -1,3 +1,23 @@
+import Navbar from "./Navbar";
+import { useState } from "react";
+
 export default function DiningPage() {
-  return <div></div>;
+  const [formData, setFormData] = useState("");
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="header">
+        <h1>Discover Your Next Meal</h1>
+        <p>Find the perfect dining experience tailored to your taste</p>
+      </div>
+      <form>
+        <input
+          placeholder="Enter Restaurant Name "
+          className=""
+          type="text"
+        ></input>
+      </form>
+    </div>
+  );
 }
