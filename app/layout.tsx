97688a2c44f1ff34edf3ccbba51e469cf22c5200
Aaron Siu <aarons3209@gmail.com>
2025-10-24 20:46:05 -0400

    Deleted unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,6 @@
 import type { Metadata } from "next";
-import Link from "next/link";
 import "./globals.css";
-import Image from "next/image";
 
-//Logo
-import smartTripLogo from "../assets/logos/smarttrip-transparent-logo.png";
-
-//outlined favicons for when not hovered over
-import flightsIconOutlinedImg from "../assets/favicons/flights-outlined-100px.png";
-import hotelsIconOutlinedImg from "../assets/favicons/hotels-outlined-100px.png";
-import carsIconOutlinedImg from "../assets/favicons/car-rental-outlined-100px.png";
-import experiencesIconOutlinedImg from "../assets/favicons/experiences-outlined-100px.png";
-
-//filled in favicons for when hovered over
-import flightsIconFilledImg from "../assets/favicons/flights-filled-in-100px.png";
-import hotelsIconFilledImg from "../assets/favicons/hotels-filled-in-100px.png";
-import carsIconFilledImg from "../assets/favicons/car-rental-filled-in-100px.png";
-import experiencesIconFilledImg from "../assets/favicons/experiences-filled-in-100px.png";
 import NavBar from "@/components/NavBar";
 
 type layoutProps = {
