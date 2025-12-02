771cd71ee4e4221e9999406a42f16608c66a98d4
Ivan Rudik <ivanrudik42@gmail.com>
2025-12-02 17:29:22 -0500

    got rid of neon and Auth0 replaced with supabase and now need to change everything else, the login should allow for gmail logins as well now but have no clue if that works because haven't tested

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Auth0Provider } from "@auth0/nextjs-auth0/client";
 import "./globals.css";
 import NavBar from "@/app/NavBar/NavBar";
 
@@ -8,7 +7,7 @@ type layoutProps = {
 };
 
 export const metadata: Metadata = {
-  title: "Smart Trip",
+  title: "SmartTrip",
   description: `SmartTrip is your central command for travel, 
     bringing every step of the planning 
     process into one seamless platform.`,
@@ -18,10 +17,8 @@ export default function RootLayout({ children }: layoutProps) {
   return (
     <html lang="en">
       <body className="bg-white">
-        <Auth0Provider>
-          <NavBar />
-          {children}
-        </Auth0Provider>
+        <NavBar />
+        {children}
       </body>
     </html>
   );
